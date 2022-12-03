a7fb8fa9d154d58992a42193caa7c0545e40477e
brianofftherails <72623210+brianofftherails@users.noreply.github.com>
2022-12-03 18:27:49 -0500

    Add files via upload

---
+++ day1_1.py
@@ -0,0 +1,18 @@
+
+try:
+    fp = open("input.txt", "r")
+except OSError:
+    print("Failed to open file")
+    exit
+all_contents = fp.readlines()
+knapsacks = list()
+knapsacks.extend([0])
+idx = 0
+for line in all_contents:
+    try:
+        knapsacks[idx] += int(line)
+    except ValueError:
+        idx += 1
+        knapsacks.extend([0])
+knapsacks.sort()
+print(sum(knapsacks[-3::]))
